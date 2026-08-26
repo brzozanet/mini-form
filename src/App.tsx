5dd777229c0268aec6fc734f6b736336c7bc5526
Paweł Brzozowski <pawel@brzoza.net>
2026-08-26 14:51:28 +0200

    Add CSS module for layout styling and restructure component rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import FormBasic from "./components/FormBasic";
 import FormRHF from "./components/FormRHF";
+import css from "./App.module.css";
 
 export default function App() {
   return (
     <>
       <h1>Mini Form (useState & RHF) ☰</h1>
-      <FormBasic />
-      <hr />
-      <FormRHF />
+      <div className={css.container}>
+        <div>
+          <FormRHF />
+        </div>
+        <hr />
+        <div>
+          <FormBasic />
+        </div>
+      </div>
     </>
   );
 }
